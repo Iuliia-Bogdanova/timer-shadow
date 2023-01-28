099ddc93ddd4e03ad5a79dec55320c106ea25028
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-01-28 20:33:45 +0400

    install react-google-charts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
+import { Chart } from "react-google-charts";
 
 function App() {
   return (
     <div className="App">
+      <Chart
+        chartType="ScatterChart"
+        data={[
+          ["Age", "Weight"],
+          [4, 5.5],
+          [8, 12],
+        ]}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
       <p>Hello Wolrd</p>
     </div>
   );
